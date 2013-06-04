8278c572341ee1c531bc21fdfeac187b6397911e
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2013-06-04 19:43:02 +0900

    added File upload 'card' with File API

--- app/l10n/ja.js
+++ app/l10n/ja.js
@@ -1,7 +1,8 @@
 define(function () {
   return {
-    "Hello world": "ハローワールド",
     "Failed fetching data": "データの取得に失敗しました",
-    "re-fetch data": "データの再取得"
+    "Failed uploading": "アップロードに失敗しました",
+    "re-fetch data": "データの再取得",
+    "uploading...": "アップロード中..."
   };
 });
