c787f0f9383e3c0265ea710e4ca87ade9b400aa8
Masahiro Iuchi <miuchi@sixapart.com>
2013-06-28 12:55:51 +0900

    Implemented dashboard widget. bugid:110038

--- plugins/Loupe/lib/Loupe/App.pm
+++ plugins/Loupe/lib/Loupe/App.pm
@@ -4,6 +4,38 @@ use warnings;
 
 use Loupe;
 
+sub send_welcome_mail_to_yourself {
+    my $app = shift;
+
+    return $app->errtrans('Invalid request.')
+        unless $app->request_method eq 'POST' && Loupe->is_enabled;
+
+    my $user = $app->user or return;
+    return $app->permission_denied
+        unless $user->is_superuser
+        || MT::Permission->count( { author_id => $user->id } );
+
+    my ( $msg_loop, $error ) = _send_mail_core( $app, [ $user->id ] );
+
+    $error ? $app->json_error(@$msg_loop) : $app->json_result();
+}
+
+sub widgets {
+    my $app = MT->app;
+    my $user = $app->user or return;
+    return if !$user->is_superuser && !Loupe->is_enabled;
+    return {
+        welcome_to_loupe => {
+            label    => 'Welcome to Loupe',
+            template => 'widget/welcome_to_loupe.tmpl',
+            handler  => sub { $_[2]->{loupe_is_enabled} = Loupe->is_enabled },
+            singular => 1,
+            set      => 'main',
+            view     => 'user',
+        },
+    };
+}
+
 sub list_actions {
     return {
         send_welcome_mail => {
@@ -15,7 +47,9 @@ sub list_actions {
                 );
             },
             condition => sub {
-                MT->app->user->is_superuser && Loupe->is_enabled;
+                my $app = MT->app;
+                my $user = $app->user or return;
+                $user && $user->is_superuser && Loupe->is_enabled;
             },
             code => \&_send_welcome_mail,
         },
@@ -37,14 +71,29 @@ sub _send_welcome_mail {
             'Cannot send welcome mail because Loupe is not enabled.')
     ) unless Loupe->is_enabled;
 
+    my @id = $app->param('id');
+    my ($msg_loop) = _send_mail_core( $app, \@id );
+
+    $plugin->load_tmpl( 'welcome_mail_result.tmpl',
+        { message_loop => $msg_loop, return_url => $app->return_uri } );
+}
+
+sub _send_mail_core {
+    my ( $app, $ids ) = @_;
+
     require MT::Mail;
+    my $plugin = MT->component('Loupe');
+    my $param  = {
+        loupe_html_url => Loupe->html_url,
+        loupe_site_url => Loupe->official_site_url,
+    };
+    my $tmpl = $plugin->load_tmpl( 'welcome_mail.tmpl', $param );
     my @msg_loop;
-    my $tmpl = $plugin->load_tmpl('welcome_mail.tmpl');
-    my @id   = $app->param('id');
-    foreach (@id) {
+    my $error;
+
+    foreach (@$ids) {
         my $author = MT::Author->load($_)
             or next;
-
         my $res;
         if ( $author->email ) {
             my %head = (
@@ -53,11 +102,7 @@ sub _send_welcome_mail {
                 From => $app->config('EmailAddressMain') || $app->user->email,
                 Subject => $plugin->translate('Welcome to Loupe.'),
             );
-            my $param = {
-                loupe_html_url => Loupe->html_url,
-                loupe_site_url => Loupe->official_site_url,
-            };
-            my $body = $app->build_page_in_mem( $tmpl, $param );
+            my $body = $app->build_page_in_mem($tmpl);
             if ( MT::Mail->send( \%head, $body ) ) {
                 $res
                     = $plugin->translate(
@@ -72,7 +117,8 @@ sub _send_welcome_mail {
                 );
             }
             else {
-                $res = $plugin->translate(
+                $error = 1;
+                $res   = $plugin->translate(
                     "Error sending e-mail ([_1]); Please fix the problem, then "
                         . "try again to recover your password.",
                     MT::Mail->errstr
@@ -87,6 +133,7 @@ sub _send_welcome_mail {
             }
         }
         else {
+            $error = 1;
             $res
                 = $plugin->translate(
                 "User '[_1]' (user #[_2]) does not have email address",
@@ -96,8 +143,7 @@ sub _send_welcome_mail {
         push @msg_loop, { message => $res };
     }
 
-    $plugin->load_tmpl( 'welcome_mail_result.tmpl',
-        { message_loop => \@msg_loop, return_url => $app->return_uri } );
+    return ( \@msg_loop, $error );
 }
 
 sub post_save_config {
