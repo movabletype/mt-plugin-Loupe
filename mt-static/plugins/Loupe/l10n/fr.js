f46ebf8bed7aeecac587b3859fca264495c1b82e
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2013-07-01 20:01:25 +0900

    update build files

--- mt-static/plugins/Loupe/l10n/fr.js
+++
@@ -1,5 +0,0 @@
-define("l10n/fr",[],function(){return{}});
-define("json!cards/acception/l10n/fr.json",function(){return{}});
-define("json!cards/feedbacks/l10n/fr.json",function(){return{}});
-define("json!cards/stats/l10n/fr.json",function(){return{}});
-define("json!cards/upload/l10n/fr.json",function(){return{}});
