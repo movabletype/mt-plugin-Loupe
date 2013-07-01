c95522b89cbd4885487ca7304a5b38d0532fad01
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2013-07-01 11:10:30 +0900

    removed temporary translation (bugid:110060)

--- build/app/l10n/ja.js
+++ build/app/l10n/ja.js
@@ -1,10 +1,3 @@
 define(function () {
-  return {
-    "Failed fetching data": "データの取得に失敗しました",
-    "Failed uploading": "アップロードに失敗しました",
-    "re-fetch data": "データの再取得",
-    "uploading...": "アップロード中...",
-    "Author": "投稿者",
-    "Share the post": "記事を告知する"
-  };
+  return {};
 });
