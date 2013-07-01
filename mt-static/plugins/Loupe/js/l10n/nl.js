4d958c7f2e2b4cbff5b836bca6450d252f9dde79
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2013-07-01 20:01:25 +0900

    update build files

---
+++ mt-static/plugins/Loupe/js/l10n/nl.js
@@ -0,0 +1,6 @@
+define("js/l10n/nl",[],function(){return{}});
+define("json!cards/acception/l10n/nl.json",function(){return{Accept:null,acception:null,"Published!":null,Published:null}});
+define("json!cards/feedbacks/l10n/nl.json",function(){return{"Another comment":null,Approve:null,"Approved!":null,Approved:null,feedbacks:null,"Post the reply":null,"Posted comment successfully":null,Reply:null,"The comment posted":null,"The post is not published":null}});
+define("json!cards/stats/l10n/nl.json",function(){return{stats:null}});
+define("json!cards/upload/l10n/nl.json",function(){return{"choose another files":null,"choose files":null,"Failed uploading":null,"more upload":null,retry:null,upload:null}});
+define("json!l10n/nl.json",function(){return{"Analytics service does not enabled":null,Author:null,Back:null,close:null,"Failed fetching data":null,Logout:null,next:null,previous:null,"re-fetch data":null,"read more":null,"Share the post":null,"User has no permission for this card":null,"You have no available item in this card":null}});
--- mt-static/plugins/Loupe/l10n/es.js
+++
@@ -1,5 +0,0 @@
-define("l10n/es",[],function(){return{}});
-define("json!cards/acception/l10n/es.json",function(){return{}});
-define("json!cards/feedbacks/l10n/es.json",function(){return{}});
-define("json!cards/stats/l10n/es.json",function(){return{}});
-define("json!cards/upload/l10n/es.json",function(){return{}});
