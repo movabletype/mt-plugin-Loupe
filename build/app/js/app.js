cb0a089a372c36b4bad064b939653420a4f8b937
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2013-10-18 19:56:52 +0900

    extend Marionette.Application to add methods which are added in initializer process before (no behavior change, just for test)

--- build/app/js/app.js
+++ build/app/js/app.js
@@ -3,136 +3,171 @@ define(['backbone', 'backbone.marionette', 'js/cards', 'js/cache', 'js/device', 
   function (Backbone, Marionette, cards, cache, device, commands, vent, AppRouter, Controller, MenuLayout, DashboardLayout, CardLayout, SigninView) {
     "use strict";
 
-    var app = new Marionette.Application();
-
-    if (DEBUG) {
-      require(['perf'], function (perf) {
-        perf.start();
-      });
-    }
-
-    app.addInitializer(function (options) {
-      options = options || {};
-      var app = this;
-      cache.set('app', 'initial', true);
-      cache.set('app', 'staticPath', $('#main-script').data('base'));
-      this.initial = true;
-      this.cards = cards.add(options.cards, true).getAll();
-      this.device = device;
-      this.router = options.router || new AppRouter({
-        controller: new Controller()
-      });
-
-      var $body = $(document.body);
-      if (this.device.platform) {
-        $body.addClass(this.device.platform);
-        if (this.device.version) {
-          $body.addClass(this.device.platform + this.device.versionShortStr);
-          $body.addClass(this.device.platform + this.device.versionStr);
-        }
-      }
-      if (this.device.browser) {
-        $body.addClass(this.device.browser);
-        if (this.device.browserVersion) {
-          $body.addClass(this.device.browser + this.device.browserVersionShortStr);
-          $body.addClass(this.device.browser + this.device.browserVersionStr);
-        }
-      }
-      commands.setHandler('app:buildMenu', function (params) {
-        app.menu.show(new MenuLayout(params));
-      });
-
-      commands.setHandler('app:beforeTransition', _.bind(function () {
-        $(document.body).addClass('onmove');
-        var $appBuilding = $('#app-building');
-        if (this.device.isAndroid || this.device.isWindowsPhone) {
-          var top = $(document.body).scrollTop();
-          $appBuilding.css({
-            top: top + 'px',
-            bottom: '-' + top + 'px'
-          });
-        }
-        $appBuilding.show();
-      }, this));
-
-      commands.setHandler('app:afterTransition', _.bind(function () {
-        $(document.body).removeClass('onmove');
-        $('#app-building').hide();
-      }, this));
-
-      commands.setHandler('move:signin', function (params) {
-        app.main.show(new SigninView(params));
-        commands.execute('app:afterTransition');
-      });
-
-      commands.setHandler('move:dashboard', function (params) {
-        params.cards = app.cards;
-        app.main.show(new DashboardLayout(params));
-        commands.execute('app:afterTransition');
-      });
-
-      commands.setHandler('app:error', function (params) {
-        app.main.show(new DashboardLayout(params));
-        commands.execute('app:afterTransition');
-      });
-
-      commands.setHandler('app:setCardViewHandler', function (card, route, callback) {
-        app.setCardViewHandler(card, route);
-        if (callback) {
-          callback();
-        }
-      });
-
-      cards.deploy().done(function () {
-        if (!Backbone.History.started) {
-          Backbone.history.start();
-        }
-        vent.trigger('app:cards:deploy:end');
-      });
-
-      vent.on('cards:add', function (addedCards) {
-        cards.deploy().done(function () {
-          commands.execute('dashboard:insertCard', addedCards)
-        });
-      });
-    });
-
-    app.stop = function () {
-      app.closeRegions();
-      vent.stopListening();
-      commands.removeAllHandlers();
-      cache.clearAll();
-    };
-
-    app.setCardViewHandler = function (card, route) {
-      commands.setHandler('move:cardView:' + card.id + ':' + route.id, function (params) {
-        if (DEBUG) {
-          console.log('move:cardView:' + card.id + ':' + route.id);
-        }
-        var path = 'cards/' + card.id + '/';
-        if (route.layout) {
-          require([path + route.layout.replace(/\.js$/, '')], function (Layout) {
-            app.main.show(new Layout(params));
+    var Loupe = Marionette.Application.extend({
+      setCardViewHandler: function (card, route) {
+        commands.setHandler('move:cardView:' + card.id + ':' + route.id, function (params) {
+          if (DEBUG) {
+            console.log('move:cardView:' + card.id + ':' + route.id);
+          }
+          var path = 'cards/' + card.id + '/';
+          if (route.layout) {
+            require([path + route.layout.replace(/\.js$/, '')], _.bind(function (Layout) {
+              this.main.show(new Layout(params));
+              commands.execute('app:afterTransition');
+            }, this));
+          } else {
+            params = _.extend(params, {
+              viewHeader: route.header,
+              viewView: route.view,
+              viewTemplate: route.template,
+              viewData: route.data
+            });
+            params.viewView = route.view;
+            this.main.show(new CardLayout(params));
             commands.execute('app:afterTransition');
+          }
+        }, this);
+      },
+      deployAddedCard: function (addedCards) {
+        if (addedCards && addedCards.length) {
+          cards.deploy().done(function () {
+            commands.execute('dashboard:insertCard', addedCards);
           });
-        } else {
-          params = _.extend(params, {
-            viewHeader: route.header,
-            viewView: route.view,
-            viewTemplate: route.template,
-            viewData: route.data
-          });
-          params.viewView = route.view;
-          app.main.show(new CardLayout(params));
-          commands.execute('app:afterTransition');
         }
-      });
-    };
+      },
+      appendDeviceClass: function (device) {
+        var $body = $(document.body);
 
-    app.addRegions({
-      main: '#app',
-      menu: '#menu'
+        if (device.platform) {
+          $body.addClass(device.platform);
+          if (device.version) {
+            $body.addClass(device.platform + device.versionShortStr);
+            $body.addClass(device.platform + device.versionStr);
+          }
+        }
+        if (device.browser) {
+          $body.addClass(device.browser);
+          if (device.browserVersion) {
+            $body.addClass(device.browser + device.browserVersionShortStr);
+            $body.addClass(device.browser + device.browserVersionStr);
+          }
+        }
+      },
+      setCommandHandlers: function () {
+        commands.setHandlers({
+          'app:buildMenu': {
+            context: this,
+            callback: function (params) {
+              this.menu.show(new MenuLayout(params));
+            }
+          },
+          'app:beforeTransition': {
+            context: this,
+            callback: function () {
+              $(document.body).addClass('onmove');
+              var $appBuilding = $('#app-building');
+              if (this.device.isAndroid || this.device.isWindowsPhone) {
+                var top = $(document.body).scrollTop();
+                $appBuilding.css({
+                  top: top + 'px',
+                  bottom: '-' + top + 'px'
+                });
+              }
+              $appBuilding.show();
+            }
+          },
+          'app:afterTransition': {
+            context: this,
+            callback: function () {
+              $(document.body).removeClass('onmove');
+              $('#app-building').hide();
+            }
+          },
+          'move:signin': {
+            context: this,
+            callback: function (params) {
+              this.main.show(new SigninView(params));
+              commands.execute('app:afterTransition');
+            }
+          },
+          'move:dashboard': {
+            context: this,
+            callback: function (params) {
+              params.cards = _.map(this.cards, function (card) {
+                return _.clone(card);
+              });
+              this.main.show(new DashboardLayout(params));
+              commands.execute('app:afterTransition');
+            }
+          },
+          'app:error': {
+            context: this,
+            callback: function (params) {
+              this.main.show(new DashboardLayout(params));
+              commands.execute('app:afterTransition');
+            }
+          },
+          'app:setCardViewHandler': {
+            context: this,
+            callback: function (card, route, callback) {
+              this.setCardViewHandler(card, route);
+              if (callback) {
+                callback();
+              }
+            }
+          }
+        });
+      },
+      stop: function () {
+        this.closeRegions();
+        cards.clearAll();
+        vent.off();
+        commands.removeAllHandlers();
+        cache.clearAll();
+        this._initCallbacks.reset();
+        Backbone.history.stop();
+        Backbone.history = new Backbone.History();
+      },
+      initialize: function (options) {
+        if (DEBUG) {
+          require(['perf'], function (perf) {
+            perf.start();
+          });
+        }
+        this.id = (new Date()).valueOf();
+        options = options || {};
+        cache.set('app', 'initial', true);
+        cache.set('app', 'staticPath', $('#main-script').data('base'));
+
+        this.initial = true;
+        this.cards = cards.add(options.cards, true).getAll();
+        this.device = device;
+
+        this.addRegions({
+          main: '#app',
+          menu: '#menu'
+        });
+
+        this.router = options.router || new AppRouter({
+          controller: new Controller()
+        });
+
+        this.appendDeviceClass(device);
+        this.setCommandHandlers();
+
+        vent.on('cards:add', this.deployAddedCard, this);
+
+        cards.deploy().done(function () {
+          if (!Backbone.History.started) {
+            Backbone.history.start();
+          }
+          vent.trigger('app:cards:deploy:end');
+        });
+      }
     });
+
+    var app = new Loupe();
+    app.addInitializer(app.initialize);
 
     return app;
   });
