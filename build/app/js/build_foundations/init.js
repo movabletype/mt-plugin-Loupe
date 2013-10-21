b53e99ef61a02cd71874c65670166f9bfbd30492
Yutaka Yamaguchi <yyamaguchi@sixapart.com>
2013-10-21 16:21:58 +0900

    use mt-static/plugin/Loupe as a base path

--- build/app/js/build_foundations/init.js
+++ build/app/js/build_foundations/init.js
@@ -1,4 +1,8 @@
+var el = document.getElementById('init-script'),
+  libPath = (el && el.dataset ? el.dataset.base : el.getAttribute('data-base')) || '.';
+
 require.config({
+  "baseUrl": libPath,
   "hbs": {
     "disableI18n": true,
     "disableHelpers": false,
